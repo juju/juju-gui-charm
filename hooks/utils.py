e4f07774fb88f6de3710565bb8ab9338c97a487d
Jeff Pihach <jeff.pihach@canonical.com>
2013-04-25 10:46:07 -0600

    reverted node to legacy ppa

--- hooks/utils.py
+++ hooks/utils.py
@@ -84,9 +84,9 @@ JUJU_GUI_DIR = os.path.join(CURRENT_DIR, 'juju-gui')
 JUJU_GUI_SITE = '/etc/apache2/sites-available/juju-gui'
 JUJU_GUI_PORTS = '/etc/apache2/ports.conf'
 JUJU_PEM = 'juju.includes-private-key.pem'
-BUILD_REPOSITORIES = ('ppa:chris-lea/node.js',)
+BUILD_REPOSITORIES = ('ppa:chris-lea/node.js-legacy',)
 DEB_BUILD_DEPENDENCIES = (
-    'bzr', 'imagemagick', 'make',  'nodejs',
+    'bzr', 'imagemagick', 'make',  'nodejs', 'npm',
 )
 DEB_STAGE_DEPENDENCIES = (
     'zookeeper',
