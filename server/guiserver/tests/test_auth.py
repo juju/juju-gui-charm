d5ffd14816881d1632b47707a61c4c5e3968283d
Francesco Banconi <francesco.banconi@canonical.com>
2013-07-24 14:36:08 +0200

    Add the auth modules.

---
+++ server/guiserver/tests/test_auth.py
@@ -0,0 +1,31 @@
+# This file is part of the Juju GUI, which lets users view and manage Juju
+# environments within a graphical interface (https://launchpad.net/juju-gui).
+# Copyright (C) 2013 Canonical Ltd.
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License version 3, as published by
+# the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranties of MERCHANTABILITY,
+# SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Tests for the Juju GUI server authentication management."""
+
+import unittest
+
+
+class AuthMiddlewareTest(unittest.TestCase):
+
+    def test_request_mismatch(self):
+        # The authentication fails if the request and response identifiers
+        # don't match.
+        pass
+
+    def test_multiple_auth_requests(self):
+        # Only the last authentication request is taken into consideration.
+        pass
