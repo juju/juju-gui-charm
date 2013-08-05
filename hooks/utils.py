9d0a2d83f0b0faa82559d4d12b8319bc040d58af
Nicola Larosa <nicola.larosa@canonical.com>
2013-08-05 17:00:27 +0200

    Restore Apache headers module handling.

--- hooks/utils.py
+++ hooks/utils.py
@@ -20,6 +20,7 @@ __all__ = [
     'AGENT',
     'APACHE',
     'APACHE_SITE',
+    'APACHE_PORTS',
     'API_PORT',
     'CURRENT_DIR',
     'HAPROXY',
@@ -483,6 +484,7 @@ def setup_apache_config(build_dir, serve_tests=False):
     with su('root'):
         run('a2dissite', 'default')
         run('a2ensite', 'juju-gui')
+        run('a2enmod', 'headers')
     log('Generating the Apache site configuration file.')
     context = {
         'port': WEB_PORT,
@@ -498,6 +500,7 @@ def remove_apache_setup():
     """Remove Apache setup."""
     log('Removing Apache setup.')
     with su('root'):
+        run('a2dismod', 'headers')
         run('a2dissite', 'juju-gui')
         run('a2ensite', 'default')
     if os.path.exists(APACHE_PORTS):
