c6200a3b8e188aaa5e0075adf9b83533a681859f
Francesco Banconi <francesco.banconi@canonical.com>
2013-08-23 16:37:05 +0200

    Implement wrap_write_message.

--- server/guiserver/utils.py
+++ server/guiserver/utils.py
@@ -22,6 +22,7 @@ import functools
 import logging
 import os
 import urlparse
+import weakref
 
 from tornado import escape
 
@@ -88,7 +89,26 @@ def request_summary(request):
 
 
 def wrap_write_message(handler):
-    return handler.write_message
+    """Wrap the write_message() method of the given handler.
+
+    The resulting function uses a weak reference to the handler, in order to
+    avoid calling the wrapped method if the handler (a WebSocket connection)
+    has been closed or garbage collected.
+
+    If the handler is still there, and the connection is still established,
+    JSON encode the received data before propagating it.
+    """
+    handler_ref = weakref.ref(handler)
+
+    def wrapped(data):
+        handler = handler_ref()
+        if (handler is None) or (not handler.connected):
+            return logging.warning(
+                'discarding message (closed connection): {}'.format(data))
+        message = escape.json_encode(data)
+        handler.write_message(message)
+
+    return wrapped
 
 
 def ws_to_http(url):
