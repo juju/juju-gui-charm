b87ba502b03226be332f5d0391f28a9e1b862af2
j.c.sackett <j.c.sackett@gmail.com>
2015-12-21 12:48:20 -0500

    Review fixes.

--- server/guiserver/manage.py
+++ server/guiserver/manage.py
@@ -1,6 +1,7 @@
 # This file is part of the Juju GUI, which lets users view and manage Juju
 # environments within a graphical interface (https://launchpad.net/juju-gui).
-# Copyright (C) 2013 Canonical Ltd.  #
+# Copyright (C) 2013 Canonical Ltd.
+#
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU Affero General Public License version 3, as published by
 # the Free Software Foundation.
@@ -137,7 +138,7 @@ def setup():
         help='Set to True to run the gui without minifiying or combining '
              'source files.')
     define('user', type=str, help='The juju environment user.')
-    define('password', type=str, help='The juju environment user.')
+    define('password', type=str, help='The juju environment password.')
     define('uuid', type=str, help='The juju environment uuid.')
     define('jujuversion', type=str, help='The jujud version.')
     # In Tornado, parsing the options also sets up the default logger.
